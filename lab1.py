4104afb22a1cbc8d77254e167e2528b7dc31c69a
Dhruvraj Singh <dhruvrajsingh@Dhruvrajs-MacBook-Pro.local>
2023-09-13 18:27:03 -0600

    modified with raw code

--- lab1.py
+++ lab1.py
@@ -2,6 +2,6 @@ import requests
 
 #print(requests.__version__)
 
-resp = requests.get("http://google.com")
+resp = requests.get("https://raw.githubusercontent.com/dhruvraj508/404-lab1/master/lab1.py")
 
 print(resp.text)
